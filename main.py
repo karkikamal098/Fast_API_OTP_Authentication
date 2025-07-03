fef9e601135159f54540d477c8e9b56348d64b00
unknown <kamaljungkarki13579@gmail.com>
2025-07-03 12:19:01 +0545

    some basic instance crerating, documentation viewing and all

--- main.py
+++ main.py
@@ -1,15 +1,38 @@
 from fastapi import FastAPI
 
+from pydantic import BaseModel
+from typing import Optional
+
+from typing import Union
+
+
 app = FastAPI()
 
 
-@app.get("/")
-def index():
-    return {"data": {"FastAPI": "0.1.0"}}
+@app.get("/blogs")
+def blogslist(limit= 10, published: bool = True, sort: str = "newest"):
+    if published:
+        return {"data": f"{limit} is the blogs of that number. And It is {published}"}
+    else:
+        return {"data": "This is the blogs section of no blogs"}
 
 
-@app.get("/about")
-def about():
-    return {"data": {"FastAPI": "0.1.0"}, "description": "A simple FastAPI application."}
+@app.get("/blogs/unpublished")
+def unpublished_blogs():
+    return {"data": {"his is the unpublished blogs section"}}
+
+@app.get("/blogs/{id}")
+def eachblog(id:int):
+    return {"data": "This is the blogs associate with id " + str(id)}
 
 
+# body request 
+class Blog(BaseModel):
+    title: str
+    body: str
+    published: Optional[bool] = None
+
+
+@app.post("/blogs")
+def create_blog(blog: Blog):
+    return {"data": "Blog created successfully", "blog": blog}
